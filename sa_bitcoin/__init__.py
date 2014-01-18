0daa680c0f7d969be743dc70ded0ccd2e6fa7984
Mark Friedenbach <mark@monetize.io>
2014-01-18 10:22:16 -0800

    Apply updates to build process and simplify setup.py script.

--- sa_bitcoin/__init__.py
+++ sa_bitcoin/__init__.py
@@ -17,24 +17,21 @@ def get_version():
 
 # ===----------------------------------------------------------------------===
 
-try:
-    from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.ext.declarative import declarative_base
 
-    Base = declarative_base()
+Base = declarative_base()
 
-    # SQLAlchemy ORM event registration
-    from sqlalchemy import event, orm
+# SQLAlchemy ORM event registration
+from sqlalchemy import event, orm
 
-    @event.listens_for(orm.Session, 'before_flush')
-    def lazy_defaults(session, flush_context, instances):
-        "Sets default values if left unspecified by the developer"
-        for target in session.new.union(session.dirty):
-            for attr in getattr(target, '__lazy_slots__', ()):
-                # This code may look like it does nothing, but in fact we are using
-                # properties to lazily generate values for some columns, so calling
-                # `getattr()` evaluates those lazy expressions. This is slightly
-                # kludgy.. but necessary as SQLAlchemy never calls `getattr()`
-                # before passing the field values to the database layer.
-                getattr(target, attr)
-except:
-    pass
+@event.listens_for(orm.Session, 'before_flush')
+def lazy_defaults(session, flush_context, instances):
+    "Sets default values if left unspecified by the developer"
+    for target in session.new.union(session.dirty):
+        for attr in getattr(target, '__lazy_slots__', ()):
+            # This code may look like it does nothing, but in fact we are using
+            # properties to lazily generate values for some columns, so calling
+            # `getattr()` evaluates those lazy expressions. This is slightly
+            # kludgy.. but necessary as SQLAlchemy never calls `getattr()`
+            # before passing the field values to the database layer.
+            getattr(target, attr)
--- setup.py
+++ setup.py
@@ -3,45 +3,29 @@
 
 import os
 
-from distutils.core import setup
+from setuptools import setup, find_packages
 
-from sa_bitcoin import get_version
+here = os.path.abspath(os.path.dirname(__file__))
+README = open(os.path.join(here, 'README.md')).read()
+CHANGES = open(os.path.join(here, 'CHANGES.md')).read()
+requires = filter(lambda r:'libs/' not in r,
+    open(os.path.join(here, 'requirements.txt')).read().split())
+packages = filter(lambda p:not p.startswith('xunit'), find_packages())
 
-# Compile the list of packages available, because distutils doesn't have an
-# easy way to do this.
-packages, data_files = [], []
-root_dir = os.path.dirname(__file__)
-if root_dir:
-    os.chdir(root_dir)
-
-for dirpath, dirnames, filenames in os.walk('sa_bitcoin'):
-    # Ignore dirnames that start with '.'
-    for i, dirname in enumerate(dirnames):
-        if dirname.startswith('.'): del dirnames[i]
-    if '__init__.py' in filenames:
-        pkg = dirpath.replace(os.path.sep, '.')
-        if os.path.altsep:
-            pkg = pkg.replace(os.path.altsep, '.')
-        packages.append(pkg)
-    elif filenames:
-        prefix = dirpath[len('sa_bitcoin')+1:] # Strip "sa_bitcoin/" or "sa_bitcoin\"
-        for f in filenames:
-            data_files.append(os.path.join(prefix, f))
-
-version = get_version().replace(' ', '-')
-setup(name='sqlalchemy-bitcoin',
-    version=version,
-    description=
+version = '0.0.3pre-alpha'
+setup(**{
+    'name': 'sqlalchemy-bitcoin',
+    'version': version,
+    'description': 
         u"A collection of tables and class mappings for storage and retrieval "
         u"of bitcoin protocol structures to a SQLAlchemy backing store.",
-    author='RokuSigma Inc.',
-    author_email='sqlalchemy-bitcoin@monetize.io',
-    url='http://www.github.com/monetizeio/sqlalchemy-bitcoin/',
-    download_url='http://pypi.python.org/packages/source/p/sqlalchemy-bitcoin/sqlalchemy-bitcoin-%s.tar.gz' % version,
-    package_dir={'sa_bitcoin': 'sa_bitcoin'},
-    packages=packages,
-    package_data={'sa_bitcoin': data_files},
-    classifiers=[
+    'long_description': README + '\n\n' + CHANGES,
+    'author':       'Monetize.io Inc.',
+    'author_email': 'support@tradecraft.io',
+    'url':          'https://tradecraft.io/',
+    'download_url': 'http://pypi.python.org/packages/source/p/sqlalchemy-bitcoin/sqlalchemy-bitcoin-%s.tar.gz' % version,
+    'packages': packages,
+    'classifiers': [
         'Development Status :: 3 - Alpha',
         'Intended Audience :: Developers',
         'License :: OSI Approved :: MIT License',
@@ -51,12 +35,5 @@ setup(name='sqlalchemy-bitcoin',
         'Topic :: Office/Business :: Financial',
         'Topic :: Software Development :: Libraries :: Python Modules',
     ],
-    install_requires=[
-        'python-bitcoin>=0.0.7',
-        'sqlalchemy>=0.8.2',
-    ],
-)
-
-#
-# End of File
-#
+    'install_requires': requires,
+})
